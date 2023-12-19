89aa18806bc5bd9997d7c674595860814803b36d
Ghada Ben Younes <ghadabenyounes7@gmail.com>
2023-12-20 00:32:02 +0100

    tp6

--- deploy/api/controllers/utilisateur.controllers.js
+++ deploy/api/controllers/utilisateur.controllers.js
@@ -4,12 +4,8 @@ const { ACCESS_TOKEN_SECRET }  = require ("../config.js");
 const jwt = require('jsonwebtoken');
 
 function generateAccessToken(user) {
-    return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: '1800s' });
+    return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: '365d' });
   }
-
-const db = require("../models");
-const Utilisateur = db.utilisateur;
-const Op = db.Sequelize.Op;
 
 // Find a single Utilisateur with an login
 exports.login = (req, res) => {
@@ -21,178 +17,69 @@ exports.login = (req, res) => {
   // Test
   let pattern = /^[A-Za-z0-9]{1,20}$/;
   if (pattern.test(utilisateur.login) && pattern.test(utilisateur.password)) {
-     Utilisateur.findOne({ where: { login: utilisateur.login } })
-    .then(data => {
-      if (data) {
-        const user = {
-          id: data.id,
-          name: data.nom,
-          email: data.email
+
+        const uuid = uuidv4 ();
+        const utilisateur = {
+          nom: "Martin",
+          prenom: "Jean",
+          login: "emma",
+          email : "martin.jean@gmail.com",
+          password : "toto",
+          id : uuid
         };
+
+        const user = {
+          id: utilisateur.id,
+          name: utilisateur.nom,
+          email: utilisateur.email
+        };
+
+        if(utilisateur.login!=req.body.login && utilisateur.password != req.body.password)
+        {
+          console.log("mauvais mdp");
+          res.send();
+        }
       
+        
         let accessToken = generateAccessToken(user);
         res.setHeader('Authorization', `Bearer ${accessToken}`);
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Cannot find Utilisateur with login=${utilisateur.login}.`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(400).send({
-        message: "Error retrieving Utilisateur with login=" + utilisateur.login
-      });
-    });
-  } else {
-    res.status(400).send({
-      message: "Login ou password incorrect" 
-    });
-  }
+
+        console.log (accessToken);
+
+      
+        res.send(utilisateur);
+    };    
 };
 
-exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.nom) {
-      res.status(400).send({
-        message: "Content can not be empty!"
-      });
-      return;
-    }
-  
-    const uuid = uuidv4 ();
-    // Create a Utilisateur
-    const utilisateur = {
+let utilisateurs = [];
+
+exports.inscrire = (req, res) => {
+  const nouveauUtilisateur = {
       nom: req.body.nom,
       prenom: req.body.prenom,
+      email: req.body.email,
       login: req.body.login,
-      email : req.body.email,
-      password : req.body.password,
-      id : uuid
-    };
-  
-  
-    const accessToken = generateAccessToken(utilisateur);
-
-    console.log (accessToken)
-    // Save Utilisateur in the database
-    Utilisateur.create(utilisateur)
-      .then(data => {
-        res.setHeader('Authorization', `Bearer ${accessToken}`);
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the Utilisateur."
-        });
-      });
+      password: req.body.password, // En production, assurez-vous de hasher le mot de passe
+      id: uuidv4()
   };
 
-// Retrieve all Utilisateurs from the database.
-exports.findAll = (req, res) => {
-    const nom = req.query.nom;
-    var condition = nom ? { nom: { [Op.like]: `%${nom}%` } } : null;
-  
-    console.log ("findAll")
+ 
+  utilisateurs.push(nouveauUtilisateur);
 
-    Utilisateur.findAll({ where: condition })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving Utilisateur."
-        });
-      });
+ 
+  const userForToken = {
+      id: nouveauUtilisateur.id,
+      email: nouveauUtilisateur.email
   };
 
-// Find a single Utilisateur with an id
-exports.findOne = (req, res) => {
-  const id = req.params.id;
+  let accessToken = generateAccessToken(userForToken);
+  res.setHeader('Authorization', `Bearer ${accessToken}`);
 
-  Utilisateur.findByPk(id)
-    .then(data => {
-      if (data) {
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Cannot find Utilisateur with id=${id}.`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving Utilisateur with id=" + id
-      });
-    });
+  console.log("Nouvel utilisateur enregistré", nouveauUtilisateur);
+  res.status(201).send(nouveauUtilisateur);
 };
 
-// Update a Utilisateur by the id in the request
-exports.update = (req, res) => {
-  const id = req.params.id;
 
-  Utilisateur.update(req.body, {
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Utilisateur was updated successfully."
-        });
-      } else {
-        res.send({
-          message: `Cannot update Utilisateur with id=${id}. Maybe Utilisateur was not found or req.body is empty!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating Utilisateur with id=" + id
-      });
-    });
-};
 
-// Delete a Utilisateur with the specified id in the request
-exports.delete = (req, res) => {
-  const id = req.params.id;
 
-  Utilisateur.destroy({
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Utilisateur was deleted successfully!"
-        });
-      } else {
-        res.send({
-          message: `Cannot delete Utilisateur with id=${id}. Maybe Utilisateur was not found!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete Utilisateur with id=" + id
-      });
-    });
-};
-
-// Delete all Utilisateur from the database.
-exports.deleteAll = (req, res) => {
-  Utilisateur.destroy({
-    where: {},
-    truncate: false
-  })
-    .then(nums => {
-      res.send({ message: `${nums} Utilisateur were deleted successfully!` });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all Utilisateurs."
-      });
-    });
-};
 
--- deploy/api/index.js
+++ deploy/api/index.js
@@ -14,30 +14,14 @@ app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
+app.use('/images', express.static('imgAng'));
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to CNAM application." });
-});
-
-const db = require("./models");
-
-db.sequelize.sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
-
-require("./routes/utilisateur.routes")(app);
+require("./routes")(app);
 
 // set port, listen for requests
 const PORT =  443;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
